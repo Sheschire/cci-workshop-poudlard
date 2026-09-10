bce89236a9de7ff7c96fcbfd9b17cb3442f4b2dc
Sheschire <theo.lemesle42@gmail.com>
2026-09-10 09:26:54 +0200

    yoyoyoyo

---
+++ build/CMakeFiles/rogue-box.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rogue-box.dir/src/main.cpp.o"
+  "CMakeFiles/rogue-box.dir/src/main.cpp.o.d"
+  "rogue-box"
+  "rogue-box.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rogue-box.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rogue-box.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rogue-box.
+# This may be replaced when dependencies are built.
